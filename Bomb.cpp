89ae6e515077e519a9ae859fb2934cf199129986
thanhlong2310 <nguyenthanhlong23102006ab@gmail.com>
2025-03-17 10:19:54 +0700

    3

--- Bomb.cpp
+++ Bomb.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <SDL_image.h>
+#include <Bomb.h>
+#include <Constants.h>
 
+Bomb::Bomb(int x, int y, SDL_Renderer* renderer)
+{
+    rect = {x, y, BOMB_SIZE, BOMB_SIZE};
+    SDL_Surface* surface = IMG_Load("assets/bomb.png");
+    if (!surface)
+    {
+        std::cout << "Failed to load bomb image! SDL_image Error: " << IMG_GetError() << std::endl;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+}
+void Bomb::update()
+{
+    rect.x -= BOMB_SPEED;
+}
+void Bomb::render(SDL_Renderer* renderer)
+{
+    SDL_RenderCopy(renderer, texture, nullptr, &rect);
+}
--- Bomb.h
+++ Bomb.h
@@ -1,1 +1,14 @@
+#ifndef BOMB_H
+#define BOMH_H
+#include <SDL.h>
 
+class Bomb
+{
+public:
+    SDL_Rect rect;
+    SDL_Texture* texture;
+    Bomb(int x, int y, SDL_Renderer* renderer);
+    void update();
+    void render(SDL_Renderer* renderer);
+};
+#endif
