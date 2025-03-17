8a4841fa5ea7bec8a8cfe3bfa4a7e4fa57667a9e
thanhlong2310 <nguyenthanhlong23102006ab@gmail.com>
2025-03-17 23:02:49 +0700

    4

--- Bird.cpp
+++ Bird.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <SDL_image.h>
+#include "Bird.h"
+#include "Constants.h"
+
+Bird::Bird(SDL_Renderer* renderer)
+{
+    rect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2, 50, 50};
+    velocity = 0;
+    SDL_Surface* surface = IMG_Load("assets/shiba.png");
+    if(!surface)
+    {
+        std::cout << "Failed to load shiba image! SDL_image Error: " << IMG_GetError() << std::endl;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+}
+void Bird::update()
+{
+    velocity += GRAVITY;
+    rect.y += velocity;
+}
+void Bird::jump()
+{
+    velocity = JUMP_FORCE;
+}
+void Bird::render(SDL_Renderer* renderer)
+{
+    SDL_RenderCopy(renderer, texture, nullptr, &rect);
+}
 
--- Bird.h
+++ Bird.h
@@ -1,1 +1,16 @@
+#ifndef BIRD_H
+#define BIRD_H
+#include <SDL.h>
 
+class Bird
+{
+public:
+    SDL_Rect rect;
+    int velocity;
+    SDL_Texture* texture;
+    Bird(SDL_Renderer* renderer);
+    void update();
+    void jump();
+    void render(SDL_Renderer* renderer);
+};
+#endif
