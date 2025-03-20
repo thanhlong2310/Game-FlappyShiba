5f43a70e3ae6b1bb59f751bf275f78cf3812853d
thanhlong2310 <nguyenthanhlong23102006ab@gmail.com>
2025-03-20 16:17:47 +0700

    8

--- Screen.cpp
+++ Screen.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <SDL_image.h>
+#include "Screen.h"
+#include "Constants.h"
 
+Screen::Screen(const char* imagePath, SDL_Renderer* renderer)
+{
+    SDL_Surface* surface = IMG_Load(imagePath);
+    if(!surface)
+    {
+        std::cout << "Failed to load screen image! SDL_image Error: " << IMG_GetError() << std::endl;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    rect = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+}
+Screen::~Screen()
+{
+    SDL_DestroyTexture(texture);
+}
+void Screen::render(SDL_Renderer* renderer)
+{
+    SDL_RenderCopy(renderer, texture, nullptr, &rect);
+}
